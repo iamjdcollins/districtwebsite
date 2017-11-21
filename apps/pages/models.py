c69a6e6a1005d9996aa6c2414bf8aed948a49ccd
iamjdcollins <jd@iamjdcollins.com>
2017-11-21 13:16:41 -0700

    Allow titles to be duplicate since URLs are still required to be unique

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -14,7 +14,7 @@ from apps.directoryentries.models import SchoolAdministrator
 # Create your models here.
 
 class Page(BasePage):
-  title = models.CharField(max_length=200, unique=True, help_text='',db_index=True)
+  title = models.CharField(max_length=200, help_text='',db_index=True)
   body = RichTextField(null=True, blank=True, help_text=PageHelp.body)
 
   page_page_node = models.OneToOneField(BasePage, db_column='page_page_node', on_delete=models.CASCADE, parent_link=True,editable=False,)
