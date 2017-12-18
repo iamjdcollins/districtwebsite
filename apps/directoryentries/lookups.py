b5c9e088fefa3b349516ffbd423193ef2751e53f
iamjdcollins <jd@iamjdcollins.com>
2017-12-18 14:29:27 -0700

    Allow to search with spaces.

--- apps/directoryentries/lookups.py
+++ apps/directoryentries/lookups.py
@@ -1,4 +1,5 @@
 from ajax_select import register
+import re
 from apps.common.classes import UUIDLookupChannel
 from apps.users.models import Employee
 
@@ -7,7 +8,8 @@ class EmployeesLookup(UUIDLookupChannel):
   model = Employee
 
   def get_query(self, q, request):
-    return self.model.objects.filter(username__icontains=q)[:10]
+    q = re.sub(' ','.*',q)
+    return self.model.objects.filter(is_active=1).filter(is_staff=1).filter(in_directory=1).filter(username__iregex=q)[:10]
 
   def format_item_display(self, item):
     return u"<span class='employee'>%s</span>" % item.username
