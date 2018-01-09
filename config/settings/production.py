4b2a2c8356926dd8007a3645086cb39b86a56a82
iamjdcollins <jd@iamjdcollins.com>
2018-01-09 12:16:39 -0700

    Add production email settings.

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,9 @@ from .base import *
 SAML2_AUTH['ASSERTION_URL'] = 'https://www2.slcschools.org'
 
 ALLOWED_HOSTS = ['www.slcschools.org','www2.slcschools.org',]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+
