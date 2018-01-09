9ad2ec0977c9af066b45f74eb07d7f2fad5a3ab9
iamjdcollins <jd@iamjdcollins.com>
2018-01-09 12:16:54 -0700

    Add development email settings.

--- config/settings/development.py
+++ config/settings/development.py
@@ -16,3 +16,6 @@ INSTALLED_APPS += [
 MIDDLEWARE += [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp'
