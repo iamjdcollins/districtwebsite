af6d1e15e8fab008ce9d1be13f086245286519db
iamjdcollins <jd@iamjdcollins.com>
2018-01-09 13:09:15 -0700

    fix smtp settings.

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,8 @@ SAML2_AUTH['ASSERTION_URL'] = 'https://www2.slcschools.org'
 ALLOWED_HOSTS = ['www.slcschools.org','www2.slcschools.org',]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
-EMAIL_PORT = '587'
+EMAIL_HOST = 'slcschools-org.mail.protection.outlook.com'
+EMAIL_PORT = '25'
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 25
 
