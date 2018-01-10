ea4f9d61fa47dcbc6a9beee13190541b62abd8bb
iamjdcollins <jd@iamjdcollins.com>
2018-01-10 11:55:04 -0700

    Add subpages to the search results.

--- apps/pages/search_indexes.py
+++ apps/pages/search_indexes.py
@@ -1,5 +1,5 @@
 from haystack import indexes
-from .models import Page, School, Department
+from .models import Page, School, Department, SubPage
 
 class PageIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -24,6 +24,15 @@ class DepartmentIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Department
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.filter(deleted=0).filter(published=1).filter(searchable=1)
+
+class SubPageIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return SubPage
 
     def index_queryset(self, using=None):
         return self.get_model().objects.filter(deleted=0).filter(published=1).filter(searchable=1)
