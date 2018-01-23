8667df48f5cd27809438fbd8351cd14c0c94170f
iamjdcollins <jd@iamjdcollins.com>
2018-01-23 10:26:20 -0700

    Fix indentation and add the site filter to the queryset.

--- apps/objects/admin.py
+++ apps/objects/admin.py
@@ -8,24 +8,33 @@ from django.contrib.admin.actions import delete_selected
 from .models import Node
 
 class NodeAdmin(MPTTModelAdmin):
-  list_per_page = 100000
-  def get_fields(self, request, obj=None):
-    if obj:
-      return ('node_title', 'menu_item', 'menu_title', 'published','searchable','parent','url',)
-    else:
-      return ('node_title', 'menu_item', 'menu_title', 'published','searchable','parent','url',)
+    list_per_page = 100000
+    def get_fields(self, request, obj=None):
+        if obj:
+            return ('node_title', 'menu_item', 'menu_title', 'published','searchable','parent','url',)
+        else:
+            return ('node_title', 'menu_item', 'menu_title', 'published','searchable','parent','url',)
 
-  def get_readonly_fields(self, request, obj=None):
+    def get_readonly_fields(self, request, obj=None):
         if obj:
             return ['parent','url',]
         else:
             return ['parent','url',]
 
-  def get_list_display(self,request):
-    if request.user.has_perm('sitestructure.restore_sitestricture'):
-      return ('node_title','node_type','content_type',)
-    else:
-      return ('node_title','node_type','content_type',)
+    def get_list_display(self,request):
+        if request.user.has_perm('sitestructure.restore_sitestricture'):
+            return ('node_title','node_type','content_type',)
+        else:
+            return ('node_title','node_type','content_type',)
+
+    def get_queryset(self, request):
+       qs = super().get_queryset(request)
+       qs = qs.filter(site=request.site.pk)
+       if request.user.is_superuser:
+           return qs
+       if request.user.has_perm('pages.restore_page'):
+           return qs
+       return qs.filter(deleted=0)
 
 # Register your models here.
 admin.site.register(Node, NodeAdmin)
