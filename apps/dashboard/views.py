7d425d626f2319c50ac956f1b0d291a684f9b5ad
iamjdcollins <jd@iamjdcollins.com>
2018-01-25 13:42:09 -0700

    Change dashboard view to a template view and setup the context.

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
+from django.apps import apps
 
 class SAMLLoginRequiredMixin(LoginRequiredMixin):
     login_url = '/accounts/login/?next=/'
@@ -15,7 +16,19 @@ class BaseURL(SAMLLoginRequiredMixin, RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         return reverse('dashboard:dashboard')
 
-class Dashboard(SAMLLoginRequiredMixin, View):
+class Dashboard(SAMLLoginRequiredMixin, TemplateView):
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Welcome to ' + request.site.name + '\'s Dashboard')
+    template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        Alias = apps.get_model('multisite','alias')
+        context['host'] = self.request.META['HTTP_HOST']
+        env = context['host'].split('.')[0]
+        if '-test' in env:
+            context['sites'] = Alias.objects.filter(domain__icontains='-test')
+        elif '-dev' in env:
+            context['sites'] = Alias.objects.filter(domain__icontains='-dev')
+        else:
+            context['sites'] = Alias.objects.filter(is_canonical=True)
+        return context
