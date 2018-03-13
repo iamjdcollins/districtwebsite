13df988292f8e0ac0f650781216fda65fcdec6b9
iamjdcollins <jd@iamjdcollins.com>
2018-03-13 14:01:27 -0600

    Pep8 fixes.

--- apps/common/middleware.py
+++ apps/common/middleware.py
@@ -1,15 +1,16 @@
 import os
 from django.conf import settings
 
+
 class DynamicDataDir(object):
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        settings.DATA_DIR = '/srv/nginx/' + request.site.domain 
+        settings.DATA_DIR = '/srv/nginx/' + request.site.domain
         settings.MEDIA_ROOT = os.path.join(settings.DATA_DIR)
         settings.STATIC_ROOT = os.path.join(settings.MEDIA_ROOT, 'static')
-        
+
         response = self.get_response(request)
 
         return response
