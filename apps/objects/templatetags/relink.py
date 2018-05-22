80ecd52bffbb9a9ee83999bb9b7eb81022bd493a
iamjdcollins <jd@iamjdcollins.com>
2018-05-22 08:30:12 -0600

    Add an images function to support updating inline images URLS.

--- apps/objects/templatetags/relink.py
+++ apps/objects/templatetags/relink.py
@@ -29,7 +29,26 @@ def linked(match, nodes=nodes_dict):
     return value
 
 
+def images(match, nodes=nodes_dict):
+    value = match.group(1)
+    if re.search('inlineimage-img', match.group(1)):
+        try:
+            id = re.search(r'data-id=\"(.*?)\"', match.group(1)).group(1)
+        except IndexError:
+            return ''
+        if id in nodes:
+            url = nodes[id]['url']
+        else:
+            url = False
+        if url:
+            value = re.sub(r'src=\".*?\"', 'src="{0}"'.format(url), value)
+        value = re.sub(r'inlineimage-img', 'inlineimage-img linked', value)
+    value = '<img {0} />'.format(value)
+    return value
+
+
 @register.filter(name='relink', is_safe=True)
 def relink(value):
     value = re.sub(r'<a (.*?)</a>', linked, value)
+    value = re.sub(r'<img (.*?) />', images, value)
     return mark_safe(value)
