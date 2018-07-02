7bd8676854879a8f48a07210456549f2cac3e546
iamjdcollins <jd@iamjdcollins.com>
2018-07-02 12:04:48 -0600

    Limit menu items to items on the current site.

--- apps/objects/processors.py
+++ apps/objects/processors.py
@@ -6,7 +6,7 @@ def breadcrumb(request):
   return {'BREADCRUMB': breadcrumb}
 
 def mainmenu(request):
-  menu_items = Node.objects.filter(menu_item=1).filter(level=0)
+  menu_items = Node.objects.filter(menu_item=1).filter(level=0).filter(site=request.site)
   return {'MENU_ITEMS': menu_items}
 
 def sitestructure(requset):
