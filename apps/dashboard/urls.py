14bcad295275cfbba11a01021ab58e23a96da7a4
iamjdcollins <jd@iamjdcollins.com>
2018-07-03 08:41:45 -0600

    Add sitestypes, sitestypes/add, sitetypes/change, and pagelayouts/change urls.

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -9,8 +9,12 @@ urlpatterns = [
   url(r'^sites/$', views.Sites.as_view(), name='sites'),
   url(r'^sites/add/$', views.SitesAdd.as_view(), name='sitesadd'),
   url(r'^sites/change/(?P<sitepk>.*)/$', views.SitesChange.as_view(), name='siteschange'),
+  url(r'^sitetypes/$', views.SiteTypes.as_view(), name='sitetypes'),
+  url(r'^sitetypes/add/$', views.SiteTypesAdd.as_view(), name='sitetypesadd'),
+  url(r'^sitetypes/change/(?P<sitetypepk>.*)/$', views.SiteTypesChange.as_view(), name='sitetypeschange'),
   url(r'^templates/$', views.Templates.as_view(), name='templates'),
   url(r'^templates/add/$', views.TemplatesAdd.as_view(), name='templatesadd'),
   url(r'^pagelayouts/$', views.PageLayouts.as_view(), name='pagelayouts'),
   url(r'^pagelayouts/add/$', views.PageLayoutsAdd.as_view(), name='pagelayoutsadd'),
+  url(r'^pagelayouts/change/(?P<pagelayoutpk>.*)/$', views.PageLayoutsChange.as_view(), name='pagelayoutschange'),
 ]
