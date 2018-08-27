6930995a5a90bda247d1cc9c6535c06247920b1e
iamjdcollins <jd@iamjdcollins.com>
2018-08-27 14:30:39 -0600

    Clean up menu_items context.

--- apps/objects/processors.py
+++ apps/objects/processors.py
@@ -10,42 +10,30 @@ def mainmenu(request):
   if request.site.domain == 'www.slcschools.org':
     menu_items = Node.objects.filter(menu_item=1).filter(level=0).filter(site=request.site).filter(published=1).filter(deleted=0)
   else:
-    menu_items = []
-    possible_menu_items = (
-      Node
-      .objects
-      .filter(
-        menu_item=1,
-        level=0,
-        site=request.site,
-        published=1,
-        deleted=0,
-      )
-    )
-    for item in possible_menu_items:
-      if (
-          request.user.is_superuser or
-          request.user.groups.filter(name='Website Managers') or
-          request.site.dashboard_sitepublisher_site.filter(account=request.user.pk)
-      ):
-        menu_items.append(item)
-      else:
-        if len(
-          item
-          .get_children()
-          .filter(
-            node_type='pages',
-            content_type='page',
-            deleted=0,
+    menu_items = (
+        Node
+        .objects
+        .filter(
+            menu_item=1,
+            level=0,
+            site=request.site,
             published=1,
-          )
-        ) > 0:
-          menu_items.append(item)
+            deleted=0,
+        )
+    )
+    if not commonfunctions.is_siteadmin(request):
+        menu_items = menu_items.filter(section_page_count__gte=1)
   return {'MENU_ITEMS': menu_items}
+
 
 def sitestructure(requset):
     sitestructure = Node.objects.all()
     return {'SITESTRUCTURE': sitestructure}
 
+
 def is_siteadmin(request):
     return {'SITEADMIN': commonfunctions.is_siteadmin(request)}
+
+
+def is_globaladmin(request):
+    return {'GLOBALADMIN': commonfunctions.is_globaladmin(request)}
